8c615daacbab656ebe75f7342b6942b298cb3b1d
moeSizlak <paul14075@gmail.com>
2018-03-05 11:41:18 -0500

    add bntech.io domain to block list

--- bnfu.js
+++ bnfu.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function() { return {cancel: true}; },
   {
-    urls: ["*://*.buffalonews.com/*paywall*", "*://*.buffalonews.com/metrics/api/*"],
+    urls: ["*://*.buffalonews.com/*paywall*", "*://*.buffalonews.com/metrics/api/*", "*://*.bntech.io/*paywall*", "*://*.bntech.io/metrics/api/*", "*://*.bntech.io/metrics/v2/*", "*://*.bntech.io/metrics/v2/js/*"],
     types: ["stylesheet", "script"]
   },
   ["blocking"]
